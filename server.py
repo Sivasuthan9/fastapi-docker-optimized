8a96f8b6a9a02dc6bf92a0de08f483205375321e
Sivasuthan9 <sivasuthans.20@uom.lk>
2025-02-11 15:12:19 +0530

    formatted

--- server.py
+++ server.py
@@ -5,11 +5,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.get("/")
 def read_root():
@@ -20,7 +21,8 @@ def read_root():
 def welcome(name: str):
     return {"message": f"Welcome {name}!"}
 
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 
+    uvicorn.run(app, host="0.0.0.0", port=8000)
